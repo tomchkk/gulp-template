c1de4ba53b478c5c03a618cd96a88689fc677c36
Tom Moore <tom@mediaskunkworks.com>
2017-01-24 10:12:08 +0000

    Modularise build task

--- gulp/tasks/build.task.js
+++ gulp/tasks/build.task.js
@@ -1,7 +1,10 @@
+var task = require('../lib/task-name')(module.filename);
 var print = require('../lib/printMsg');
 
 module.exports = function (gulp, options, $) {
-  gulp.task('build', function () {
+  var config = options.config.tasks[task];
+
+  gulp.task(task, config.deps || null, function () {
     // console.log('These are your options: ', options);
     print('Hello world!');
     return gulp.src('./src/**/*.js')
