c1ea04510f13357aa2c84b4e50015064731fae7f
Tom Moore <tom@mediaskunkworks.com>
2017-02-10 11:33:26 +0000

    Add default dev config

--- gulp/tasks/build.task.js
+++ gulp/tasks/build.task.js
@@ -1,13 +1,14 @@
 var task = require('../lib/task-name')(module.filename);
-var print = require('../lib/printMsg');
 
 module.exports = function (gulp, options, $) {
   var config = options.config.tasks[task];
 
   gulp.task(task, config.deps || null, function () {
-    // console.log('These are your options: ', options);
-    print('Hello world!');
-    return gulp.src('./src/**/*.js')
-      .pipe($.debug());
+    console.log('Hello, World!');
+    console.log('gulp.dest: \'%s\'', config.dest);
+
+    return gulp.src(config.src)
+      .pipe($.debug())
+      .pipe(gulp.dest(config.dest));
   });
 };
