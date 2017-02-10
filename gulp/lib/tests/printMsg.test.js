a66e99332a7257b994401e2f13677a3c2cfa6127
Tom Moore <tom@mediaskunkworks.com>
2017-02-10 11:34:06 +0000

    Remove dev lib

--- gulp/lib/tests/printMsg.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var print = require('../printMsg');
-
-describe('printMsg', function () {
-  it('prints a given string', function () {
-    var msg = print('this is a test');
-    assert.equal(msg, 'this is a test');
-  });
-});
