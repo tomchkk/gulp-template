c235ee7338572dafeb5e930c2b68b7e2056427c6
Tom Moore <tom@mediaskunkworks.com>
2017-02-10 11:08:51 +0000

    Improve comment in watch task

--- gulp/tasks/watch.task.js
+++ gulp/tasks/watch.task.js
@@ -4,7 +4,7 @@ module.exports = function (gulp, options, $) {
   var config = options.config.tasks[task];
 
   gulp.task(task, config.deps || null, function () {
-    // get an array of watch tasks and, for each task use their src as watch's src
+    // get an array of watch tasks and, for each task, use their src as watch's src
     config.tasks.forEach(function (watchTask) {
       gulp.watch(options.config.tasks[watchTask].src, [watchTask]);
     });
