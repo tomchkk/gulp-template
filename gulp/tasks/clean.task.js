2d90c263ea1faf5cfb26a2893a29cd171538cde3
Tom Moore <tom@mediaskunkworks.com>
2017-04-06 12:30:02 +0100

    Refactor clean task

--- gulp/tasks/clean.task.js
+++ gulp/tasks/clean.task.js
@@ -1,5 +1,33 @@
-var task = require('../lib/task-name')(module.filename);
 var del = require('del');
+var task = require('../lib/task-name')(module.filename);
+
+module.exports = function (gulp, options, $) {
+  var config = options.config.tasks[task];
+
+  gulp.task(task, config.deps || null, function () {
+    // clean 'dest' directories for each configured task
+    config.dest = getUniqueDest(gulp.seq, options.config.tasks);
+    return del.sync(config.dest, {read: false});
+  });
+};
+
+/**
+ * Get a unique array of existing [task].dest values for each memeber of 'seq' that
+ * isn't this task.
+ * @param  {Array}  seq   A gulp sequence (gulp.seq) array
+ * @param  {Object} tasks An object containing defined gulp tasks
+ * @return {Array}        An array of unique task 'dest' values
+ */
+function getUniqueDest (seq, tasks) {
+  return Array.from(new Set(
+    seq.filter(excludeTask)
+      .map(function (t) {
+        return tasks[t].dest || null;
+      })
+      .filter(excludeNull)
+      .filter(excludeCWD)
+  ));
+}
 
 var excludeTask = function (value) {
   return value !== task;
@@ -9,14 +37,6 @@ var excludeNull = function (val) {
   return val !== null;
 };
 
-module.exports = function (gulp, options, $) {
-  var config = options.config.tasks[task];
-
-  gulp.task(task, config.deps || null, function () {
-    // build an array of existing [task].dest values for each member of gulp.seq that isn't this task
-    config.dest = gulp.seq.filter(excludeTask).map(function (task) {
-      return options.config.tasks[task].dest || null;
-    }).filter(excludeNull);
-    return del.sync(config.dest, {read: false});
-  });
+var excludeCWD = function (dest) {
+  return dest !== './';
 };
